4361451e83867bad6ebb788e363c0f790ceb3f08
Cristian <cristianege52@gmail.com>
2026-08-24 23:38:05 -0600

    fix: crear vista personalizada para servir React
    - Vista serve_react para archivos estaticos y SPA
    - Corregir servido de index.html y assets

--- backend/urls.py
+++ backend/urls.py
@@ -1,22 +1,34 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.static import serve
+from django.http import HttpResponse, FileResponse
 from django.conf import settings
+import os
+
+def serve_react(request, path=''):
+    """Sirve archivos del build de React"""
+    react_dir = settings.REACT_APP_DIR
+    
+    # Si se pide un archivo especifico, servirlo
+    if path:
+        file_path = os.path.join(react_dir, path)
+        if os.path.isfile(file_path):
+            return FileResponse(open(file_path, 'rb'))
+    
+    # Para todo lo demas, servir index.html
+    index_path = os.path.join(react_dir, 'index.html')
+    if os.path.isfile(index_path):
+        return FileResponse(open(index_path, 'rb'))
+    
+    return HttpResponse('Archivo no encontrado', status=404)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('documentos.urls')),
 ]
 
-# Servir archivos estaticos del build de React
-reac_dist = settings.REACT_APP_DIR
+# React: servir archivos estaticos y SPA
 urlpatterns += [
-    re_path(r'^assets/(?P<path>.*)$', serve, {'document_root': reac_dist / 'assets'}),
-    re_path(r'^favicon\.svg$', serve, {'document_root': reac_dist / 'favicon.svg'}),
-    re_path(r'^icons\.svg$', serve, {'document_root': reac_dist / 'icons.svg'}),
-]
-
-# React: servir index.html para todo lo demas
-urlpatterns += [
-    re_path(r'^(?!api/).*$', serve, {'document_root': reac_dist, 'path': 'index.html'}),
+    re_path(r'^assets/(?P<path>.*)$', lambda r, path: serve_react(r, f'assets/{path}')),
+    re_path(r'^(?P<path>.+\.(js|css|svg|png|jpg|ico|json))$', lambda r, path: serve_react(r, path)),
+    re_path(r'^(?!api/).*$', serve_react),
 ]
