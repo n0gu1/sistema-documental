ddfb7806e4c61dc78030da5ad722065612d5d731
Cristian <cristianege52@gmail.com>
2026-08-27 19:23:31 -0600

    corregir compilacion del frontend en Render

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Build script for Render
+npm ci --prefix frontend --include=dev
+npm run build --prefix frontend
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
